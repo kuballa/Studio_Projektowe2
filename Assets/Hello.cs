f9db2abd5f2a71521effcf2db7e69e838dfa2fc6
Jakub <jziomek669@gmail.com>
2023-03-17 11:20:25 +0100

    add unity files

---
+++ Assets/Hello.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Debug.Log("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Hello World!");
+        }
+    }
+}
