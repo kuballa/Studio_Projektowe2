02656d2aaa2c1bd1f257411f57e3c019b031767c
zarebajakub <jakubzareba2001@gmail.com>
2023-04-24 14:03:09 +0200

    Update Hello.cs
    changed generation to 3d grid, attempt to do comparing neighbourhood

--- Assets/Hello.cs
+++ Assets/Hello.cs
@@ -7,160 +7,147 @@ using System.Linq;
 using UnityEngine;
 using Color = UnityEngine.Color;
 
+public struct CubeNeighbourhood
+{
+    // to think about - what type
+    //public int top;
+    //public int bottom;
+    public int leftSide, rightSide, frontSide, backSide;
+
+    public CubeNeighbourhood(int l, int r, int f, int b)
+    {
+        leftSide = l;
+        rightSide = r;
+        frontSide = f;
+        backSide = b;
+    }
+}
+
 public class Hello : MonoBehaviour
 {
-    static int gridSize = 10;
-    Renderer[,] rendererGrid = new Renderer[gridSize, gridSize];
-    Color[] colors = { Color.red, Color.yellow, Color.green, Color.blue, Color.gray };
-    List<int> colorsIDs = new List<int> { 0, 1, 2, 3, 4 };
-    bool[,] finished = new bool[gridSize, gridSize];
+    CubeNeighbourhood greenNeighbour = new CubeNeighbourhood(3, 2, 3, 2);
+    CubeNeighbourhood blueNeighbour = new CubeNeighbourhood(2, 1, 2, 1);
+    CubeNeighbourhood grayNeighbour = new CubeNeighbourhood(1, 1, 1, 1);
+
+    static int[] gridSize = new int[3] {3,1,3}; // height is second!!
+    Renderer[,,] rendererGrid = new Renderer[gridSize[0], gridSize[1], gridSize[2]];
+    Color[] colors = { Color.green, Color.blue, Color.gray };
+    List<int> colorsIDs = new List<int> { 0, 1, 2 };
+    List<int>[,,] possibleOutputs = new List<int>[gridSize[0], gridSize[1], gridSize[2]];
 
     void Start()
     {
         Debug.Log("Starting...");
-        GenerateGrid();
 
-        //StartCoroutine(PaintGrid());
-
-        StartCoroutine(WaveFunctionCollapse());
+        GenerateGrid3D();
+        WaveFunctionCollapse3D();
     }
 
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.R))
+            WaveFunctionCollapse3D();
     }
 
-    void GenerateGrid()
+    void GenerateGrid3D()
     {
         Debug.Log("Generating grid...");
 
-        for (int i = 0; i < gridSize; i++)
-        {
-            for (int j = 0; j < gridSize; j++)
-            {
-                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                cube.transform.position = new Vector3(i, 0, j);
+        for (int i = 0; i < gridSize[0]; i++)
+            for (int j = 0; j < gridSize[1]; j++)
+                for (int k = 0; k < gridSize[2]; k++)
+                {
+                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.position = new Vector3(i, j, k);
 
-                var cubeRenderer = cube.GetComponent<Renderer>();
-                rendererGrid[i, j] = cubeRenderer;
-            }
-        }
+                    var cubeRenderer = cube.GetComponent<Renderer>();
+                    rendererGrid[i, j, k] = cubeRenderer;
+                }
 
         Debug.Log("Grid generation finished.");
     }
 
-    IEnumerator PaintGrid()
-    {
-        Debug.Log("Starting coloring in 3s...");
-        yield return new WaitForSeconds(3);
-
-        for (int i = 0; i < gridSize; i++)
-            for (int j = 0; j < gridSize; j++)
-            {
-                Color col = new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value);
-                rendererGrid[i,j].material.SetColor("_Color", col);
-
-                yield return new WaitForSeconds(0.1f);
-            }
-    }
-
-    IEnumerator WaveFunctionCollapse()
+    public void WaveFunctionCollapse3D()
     {
         // creating array of possibilities
-        List<int>[,] possibleOutputs = new List<int>[gridSize, gridSize];
-        for (int i = 0; i < gridSize; i++)
-            for (int j = 0;j < gridSize; j++)
-                possibleOutputs[i,j] = colorsIDs;
+        for (int i = 0; i < gridSize[0]; i++)
+            for (int j = 0; j < gridSize[1]; j++)
+                for (int k = 0; k < gridSize[2]; k++)
+                    possibleOutputs[i, j, k] = colorsIDs;
 
         System.Random random = new System.Random();
-        
-        //while (true)
-        for (int xd=0; xd < gridSize*10; xd++)
-        {
-            yield return new WaitForSeconds(0.4f);
 
+        int lowest = 0;
+        while (lowest != 999)
+        {
             // let's find out what is the lowest number of possibilities...
-            int lowest = possibleOutputs[0,0].Count();
-            for (int i = 0; i < gridSize; i++)
-                for (int j = 0; j < gridSize; j++)
-                    if (lowest > possibleOutputs[i, j].Count() && !finished[i, j])
-                        lowest = possibleOutputs[i, j].Count();
+            lowest = 999;
+            for (int i = 0; i < gridSize[0]; i++)
+                for (int j = 0; j < gridSize[1]; j++)
+                    for (int k = 0; k < gridSize[2]; k++)
+                        if (lowest > possibleOutputs[i, j, k].Count() && possibleOutputs[i, j, k].Count() != 1)
+                            lowest = possibleOutputs[i, j, k].Count();
 
             // ...and put them into a list
-            var lowList = new List<(int, int)>();
-            for (int i = 0; i < gridSize; i++)
-                for (int j = 0; j < gridSize; j++)
-                    if (lowest == possibleOutputs[i, j].Count())
-                        lowList.Add((i, j));
+            var lowList = new List<(int, int, int)>();
 
+            for (int i = 0; i < gridSize[0]; i++)
+                for (int j = 0; j < gridSize[1]; j++)
+                    for (int k = 0; k < gridSize[2]; k++)
+                        if (lowest == possibleOutputs[i, j, k].Count())
+                            lowList.Add((i, j, k));
+
+            if (lowList.Count == 0)
+                continue;
+    
             // now let's pick one randomly, and choose it's value
             int randomIndex = random.Next(lowList.Count());
-            (int,int) chosenOne = lowList[randomIndex];
+            (int, int, int) id = lowList[randomIndex];
 
-            int randomColorIndex = random.Next(possibleOutputs[chosenOne.Item1, chosenOne.Item2].Count());
-            int colorIndex = possibleOutputs[chosenOne.Item1, chosenOne.Item2][randomColorIndex];
-            possibleOutputs[chosenOne.Item1, chosenOne.Item2] = new List<int> { colorIndex };
+            int randomColorIndex = random.Next(possibleOutputs[id.Item1, id.Item2, id.Item3].Count());
+            int colorIndex = possibleOutputs[id.Item1, id.Item2, id.Item3][randomColorIndex];
+            possibleOutputs[id.Item1, id.Item2, id.Item3] = new List<int> { colorIndex };
 
-            rendererGrid[chosenOne.Item1, chosenOne.Item2].material.SetColor("_Color", colors[colorIndex]);
-            finished[chosenOne.Item1, chosenOne.Item2] = true;
+            rendererGrid[id.Item1, id.Item2, id.Item3].material.SetColor("_Color", colors[colorIndex]);
 
-            // we have to update possibilities based on what we want - first - adding neighbours which obviously are affected
-            Stack<(int, int)> toUpdate = new Stack<(int, int)>();
-            toUpdate.Push((chosenOne.Item1, chosenOne.Item2));
-
-            // right after - loop of changes - it has to be customized! right now it is only example of usage to get started with
-            while (toUpdate.Count > 0)
-            {
-                var itemToUpdate = toUpdate.Pop();
-                if (possibleOutputs[itemToUpdate.Item1, itemToUpdate.Item2].Count() == 1)
-                {
-                    int colorToDelete = possibleOutputs[itemToUpdate.Item1, itemToUpdate.Item2][0];
-                    Stack<(int, int)> neighbours = GetNeighbours(itemToUpdate.Item1, itemToUpdate.Item2);
-                    
-                    while(neighbours.Count > 0)
-                    {
-                        (int, int) nghb = neighbours.Pop();
-                        if (possibleOutputs[nghb.Item1, nghb.Item2].Contains(colorToDelete))
-                        {
-                            var newValues = new List<int> (possibleOutputs[nghb.Item1, nghb.Item2]);
-                            possibleOutputs[nghb.Item1, nghb.Item2] = new List<int> ( GetPossibleColors(newValues, colorToDelete) );
-                            toUpdate.Push(nghb);
-                        }
-                    }
-                }
-            }
+            UpdateWhileChanges();
         }
     }
 
-    private Stack<(int, int)> GetNeighbours(int a, int b)
+    public void UpdateWhileChanges()
     {
-        Stack<(int, int)> up = new Stack<(int, int)>();
 
-        if (a - 1 >= 0)
-            up.Push((a - 1, b));
-        if (a + 1 < gridSize)
-            up.Push((a + 1, b));
-        if (b - 1 >= 0)
-            up.Push((a, b - 1));
-        if (b + 1 < gridSize)
-            up.Push((a, b + 1));
-
-
-
-        return up;
     }
+    
+        //private Stack<(int, int)> GetNeighbours(int a, int b)
+        //{
+        //    Stack<(int, int)> up = new Stack<(int, int)>();
 
-    private List<int> GetPossibleColors(List<int> values, int color)
-    {
-        List<int> vals = new List<int>();
-        if (values.Contains(color-1))
-            vals.Add(color-1);
-        if (values.Contains(color+1))
-            vals.Add(color+1);
+        //    if (a - 1 >= 0)
+        //        up.Push((a - 1, b));
+        //    if (a + 1 < gridSize)
+        //        up.Push((a + 1, b));
+        //    if (b - 1 >= 0)
+        //        up.Push((a, b - 1));
+        //    if (b + 1 < gridSize)
+        //        up.Push((a, b + 1));
 
-        if (vals.Count == 0)
-            throw new NotImplementedException();
 
-        return vals;
-    }
+
+        //    return up;
+        //}
+
+    //private List<int> GetPossibleColors(List<int> values, int color)
+    //{
+    //    List<int> vals = new List<int>();
+    //    if (values.Contains(color-1))
+    //        vals.Add(color-1);
+    //    if (values.Contains(color+1))
+    //        vals.Add(color+1);
+
+    //    if (vals.Count == 0)
+    //        throw new NotImplementedException();
+
+    //    return vals;
+    //}
 }
